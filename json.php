180383ffb243e584c3dff0d82f63943d6b289fe0
aleha-druga <aleha.druga@gmail.com>
2018-07-05 21:16:03 +0500

    mido: 05.07.2018

--- json.php
+++ json.php
@@ -9,5 +9,7 @@
     ],
 ".\/mido":
     [
+        {"filename":".\/mido\/LiteOS-8.1.0-20180705-mido-OFFICIAL-OREO.zip","timestamp":1530774318},
+        {"filename":".\/mido\/LiteOS-8.1.0-20180705-mido-OFFICIAL-OREO.zip.md5sum","timestamp":1530774318}
     ]
 }
