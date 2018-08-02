69803bb0fdba7acc79e3b9c0032573568812d2b0
verevka <verevka86@gmail.com>
2018-08-02 09:55:35 +0300

    sagit: 28.07.2018

--- json.php
+++ json.php
@@ -13,7 +13,9 @@
         {"filename":".\/sagit\/LiteOS-8.1.0-20180629-sagit-OFFICIAL-OREO.zip","timestamp":1530285299},
         {"filename":".\/sagit\/LiteOS-8.1.0-20180629-sagit-OFFICIAL-OREO.zip.md5sum","timestamp":1530285299},
         {"filename":".\/sagit\/LiteOS-8.1.0-20180708-sagit-OFFICIAL-OREO.zip","timestamp":1531069132},
-        {"filename":".\/sagit\/LiteOS-8.1.0-20180708-sagit-OFFICIAL-OREO.zip.md5sum","timestamp":1531069132}
+        {"filename":".\/sagit\/LiteOS-8.1.0-20180708-sagit-OFFICIAL-OREO.zip.md5sum","timestamp":1531069132},
+        {"filename":".\/sagit\/LiteOS-8.1.0-20180728-sagit-OFFICIAL-OREO.zip","timestamp":1532772514},
+        {"filename":".\/sagit\/LiteOS-8.1.0-20180728-sagit-OFFICIAL-OREO.zip.md5sum","timestamp":1532772514}
     ],
 ".\/mido":
     [
